cc65a6523871f5a7f4461fdd957f95813a43703d
SVirchenko <pP1234567>
2017-11-14 14:30:20 +0200

    git init commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars')
+		.create({defaultLayout: 'main'});
+var fortunes = [    
+	"Победи свои страхи, или они победят тебя.",    
+	"Рекам нужны истоки.",    
+	"Не бойся неведомого.",    
+	"Тебя ждет приятный сюрприз.",    
+	"Будь проще везде, где только можно."
+];
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars')
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(express.static(__dirname + '/public')); 
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
+})
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+})
+
+app.listen(app.get('port'), function() {
+	console.log('Express listening on http://localhost:' + app.get('port') + '; pres Ctrl + C to finish.')
+})
